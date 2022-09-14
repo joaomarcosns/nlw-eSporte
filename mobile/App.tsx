b91ebe32af0b7fb6c24d0234825f396277832927
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-09-14 17:20:36 -0300

    feat: Finalizando a interface home

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,34 @@
 
-import { View } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter';
+import { StatusBar } from 'react-native';
+import { Background } from './src/components/Background';
+import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  })
+
+
   return (
-    <View>
-    </View>
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor='transparent'
+        translucent
+      />
+      {fontsLoaded ? <Home /> : <Loading />}
+    </Background>
   );
 }
