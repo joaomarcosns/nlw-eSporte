6d46039c084b3729bf1be4c3d0ba796bab18f4ef
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-09-15 09:43:15 -0300

    refactor: Adicacao do uuid nos ids

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,26 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/ads', (req, res) => {
-    return res.json([
-        {id: 1, name: 'Anuncio 1'},
-        {id: 2, name: 'Anuncio 2'},
-        {id: 3, name: 'Anuncio 3'},
-        {id: 4, name: 'Anuncio 4'},
-    ])
-})
+app.get("/games", (req, res) => {
+  return res.json([]);
+});
 
-app.listen(3333)
+app.post("/ads", (req, res) => {
+  return res.json([]);
+});
+
+app.get("/games/:id/ads", (req, res) => {
+  return res.json([
+    { id: 1, name: "Anuncio 1" },
+    { id: 2, name: "Anuncio 2" },
+    { id: 3, name: "Anuncio 3" },
+    { id: 4, name: "Anuncio 4" },
+  ]);
+});
+
+app.get("/ads/:id/discord", (req, res) => {
+  return res.json([]);
+});
+
+app.listen(3333);
